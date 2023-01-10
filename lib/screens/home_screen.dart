bd40d71409263e32598d17fa3abf35c1888a9c1f
Brayan Garcia <bgarcia.95@outlook.com>
2023-01-10 12:09:47 -0600

    add layout with card swiper and movie slider widgets

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:movies_app/widgets/widgets.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -6,9 +7,22 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        child: const Center(
-          child: Text('Home Screen'),
+      appBar: AppBar(
+        title: const Text('Movies in Theaters'),
+        elevation: 0,
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: const Icon(Icons.search_outlined),
+          )
+        ],
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: const [
+            CardSwiper(),
+            MovieSlider(),
+          ],
         ),
       ),
     );
