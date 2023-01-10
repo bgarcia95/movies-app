1b4fa2e19404ccc9ebfe229ccc60211de9d16f45
Brayan Garcia <bgarcia.95@outlook.com>
2023-01-10 15:56:36 -0600

    setup provider

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,28 @@
 import 'package:flutter/material.dart';
 import 'package:movies_app/screens/screens.dart';
 import 'package:movies_app/theme/theme.dart';
+import 'package:provider/provider.dart';
 
-void main() => runApp(const MyApp());
+import 'package:movies_app/providers/movies_provider.dart';
+
+void main() => runApp(const AppState());
+
+class AppState extends StatelessWidget {
+  const AppState({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(
+          create: (context) => MoviesProvider(),
+          lazy: false,
+        ),
+      ],
+      child: const MyApp(),
+    );
+  }
+}
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
