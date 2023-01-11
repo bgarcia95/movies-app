c6ee4613b5c78002de5bb007444ced3c01396a79
Brayan Garcia <bgarcia.95@outlook.com>
2023-01-11 15:12:13 -0600

    set custom heroId and wrap fadeinimage in hero widget

--- lib/search/search_delegate.dart
+++ lib/search/search_delegate.dart
@@ -77,11 +77,16 @@ class _MovieItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    movie.heroId = 'search-${movie.id}';
+
     return ListTile(
-      leading: FadeInImage(
-        placeholder: const AssetImage('assets/no-image.jpg'),
-        image: NetworkImage(movie.fullPosterImg),
-        fit: BoxFit.contain,
+      leading: Hero(
+        tag: movie.heroId!,
+        child: FadeInImage(
+          placeholder: const AssetImage('assets/no-image.jpg'),
+          image: NetworkImage(movie.fullPosterImg),
+          fit: BoxFit.contain,
+        ),
       ),
       title: Text(movie.title),
       subtitle: Text(movie.originalTitle),
