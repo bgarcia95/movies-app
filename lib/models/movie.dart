d0281b27fb8bc85b7edf507e60a0a85feda7fcf6
Brayan Garcia <bgarcia.95@outlook.com>
2023-01-11 13:25:30 -0600

    add backdropImg getter

--- lib/models/movie.dart
+++ lib/models/movie.dart
@@ -39,6 +39,14 @@ class Movie {
     return 'https://i.stack.imgur.com/GNhx0.png';
   }
 
+  get backdropImg {
+    if (backdropPath != null) {
+      return 'https://image.tmdb.org/t/p/w500$backdropPath';
+    }
+
+    return 'https://i.stack.imgur.com/GNhx0.png';
+  }
+
   factory Movie.fromRawJson(String str) => Movie.fromJson(json.decode(str));
 
   factory Movie.fromJson(Map<String, dynamic> json) => Movie(
