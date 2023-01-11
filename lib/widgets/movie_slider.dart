3787d558d41128918f1f737ff27caafebc0bf4c5
Brayan Garcia <bgarcia.95@outlook.com>
2023-01-11 12:34:45 -0600

    transform to stateful widget to handle listview controller. receive onNextPage as a parameter.  add code to handle infinite scroll.

--- lib/widgets/movie_slider.dart
+++ lib/widgets/movie_slider.dart
@@ -1,11 +1,49 @@
 import 'package:flutter/material.dart';
 import 'package:movies_app/models/models.dart';
 
-class MovieSlider extends StatelessWidget {
+class MovieSlider extends StatefulWidget {
   final List<Movie> movies;
   final String? title;
+  final Function onNextPage;
 
-  const MovieSlider({super.key, required this.movies, this.title});
+  const MovieSlider(
+      {super.key, required this.movies, this.title, required this.onNextPage});
+
+  @override
+  State<MovieSlider> createState() => _MovieSliderState();
+}
+
+class _MovieSliderState extends State<MovieSlider> {
+  final ScrollController scrollController = ScrollController();
+
+  @override
+  void initState() {
+    super.initState();
+    double? maxScrollTemp;
+
+    scrollController.addListener(() {
+      double positionScroll = scrollController.position.pixels;
+      double maxScroll = scrollController.position.maxScrollExtent;
+
+      if (positionScroll >= maxScroll - 600) {
+        // If MaxScrollTemp is null is because onNextPage() has not being called
+        // Otherwsise will be true if maxScrollTemp is equal to maxScroll
+        if (maxScrollTemp == maxScroll) {
+          return;
+        } else {
+          widget.onNextPage();
+          print('triggered');
+          // We store the value from maxScroll value when calling onNextPage
+          maxScrollTemp = maxScroll;
+        }
+      }
+    });
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -15,11 +53,11 @@ class MovieSlider extends StatelessWidget {
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          if (title != null)
+          if (widget.title != null)
             Padding(
               padding: const EdgeInsets.symmetric(horizontal: 20),
               child: Text(
-                title!,
+                widget.title!,
                 style:
                     const TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
               ),
@@ -28,10 +66,11 @@ class MovieSlider extends StatelessWidget {
           Expanded(
             // a listview needs to know the parents height in order to display content otherwise will throw an error. In this case the column was taking the size of its children therefore the listview didn't have a size specified and was the reason we wrapped the listview within a expended widget to take all the available space.
             child: ListView.builder(
+              controller: scrollController,
               scrollDirection: Axis.horizontal,
-              itemCount: movies.length,
+              itemCount: widget.movies.length,
               itemBuilder: (context, index) => _MoviePoster(
-                movies[index],
+                widget.movies[index],
               ),
             ),
           )
