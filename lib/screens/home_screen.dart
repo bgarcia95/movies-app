fcada32ad5c10ad6c3fd370c134072fb1961dd41
Brayan Garcia <bgarcia.95@outlook.com>
2023-01-11 11:11:27 -0600

    access to movies provider and pass now playing movies to card swiper

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,11 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:movies_app/providers/movies_provider.dart';
 import 'package:movies_app/widgets/widgets.dart';
+import 'package:provider/provider.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
+    // here we are saying: go to the widgets tree, find the first instance of MoviesProvider and bring it here
+    final moviesProvider = Provider.of<MoviesProvider>(context);
+
     return Scaffold(
       appBar: AppBar(
         title: const Text('Movies in Theaters'),
@@ -19,9 +24,9 @@ class HomeScreen extends StatelessWidget {
       ),
       body: SingleChildScrollView(
         child: Column(
-          children: const [
-            CardSwiper(),
-            MovieSlider(),
+          children: [
+            CardSwiper(movies: moviesProvider.nowPlayingMovies),
+            const MovieSlider(),
           ],
         ),
       ),
