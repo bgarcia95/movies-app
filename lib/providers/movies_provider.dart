7d0b4e089252ef69fd045322b65f4c4ce84c6e70
Brayan Garcia <bgarcia.95@outlook.com>
2023-01-11 10:50:06 -0600

    make http request to fetch now playing movies

--- lib/providers/movies_provider.dart
+++ lib/providers/movies_provider.dart
@@ -1,6 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+import 'package:movies_app/models/models.dart';
 
 class MoviesProvider extends ChangeNotifier {
+  String _apiKey = 'f4d30dfea84b32afbba315fe5d1ed849';
+  String _baseURL = 'api.themoviedb.org';
+  String _language = 'en-EN';
+
   MoviesProvider() {
     print('MoviesProvider initialized');
 
@@ -8,6 +15,15 @@ class MoviesProvider extends ChangeNotifier {
   }
 
   getNowPlayingMovies() async {
-    print('getOnDisplayMovies');
+    var url = Uri.https(_baseURL, '3/movie/now_playing', {
+      'api_key': _apiKey,
+      'language': _language,
+      'page': '1',
+    });
+
+    final response = await http.get(url);
+    final nowPlayingResponse = NowPlayingResponse.fromRawJson(response.body);
+
+    print(nowPlayingResponse.results?[1]?.title);
   }
 }
